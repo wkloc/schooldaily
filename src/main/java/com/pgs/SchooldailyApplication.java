60af4b9238dd5c1867599d3b3bf6384f1de29608
Wojciech Kloc (wkloc) <wkloc@pgs.soft.com>
2017-02-08 12:17:54 +0100

    test issue

--- src/main/java/com/pgs/SchooldailyApplication.java
+++ src/main/java/com/pgs/SchooldailyApplication.java
@@ -8,6 +8,6 @@ public class SchooldailyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchooldailyApplication.class, args);
-		//test
+		//this is test change to test jenkins build trigger
 	}
 }
