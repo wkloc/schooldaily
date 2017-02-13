ebad3dc0ab7e8ffe0354c395484243af26c3ea9a
Wojciech Kloc (wkloc) <wkloc@pgs.soft.com>
2017-02-13 15:47:31 +0100

    including Jacoco Maven plugin - configuration

--- src/test/java/com/pgs/SchooldailyApplicationTest.java
+++ src/test/java/com/pgs/SchooldailyApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SchooldailyApplicationTests {
+public class SchooldailyApplicationTest {
 
 	@Test
 	@Ignore
