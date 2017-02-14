71441fca81b48a512a37e4e0a93e229273c0b2b8
Wojciech Kloc (wkloc) <wkloc@pgs.soft.com>
2017-02-14 07:57:59 +0100

    including Jacoco Maven plugin - configuration

--- src/test/java/com/pgs/integration/integrationOneIT.java
+++ src/test/java/com/pgs/integration/integrationOneIT.java
@@ -1,7 +1,24 @@
 package com.pgs.integration;
 
+import com.pgs.One;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by wkloc on 2017-02-13.
  */
 public class integrationOneIT {
+    @Test
+    public void testFoo() throws Exception {
+        One one = new One();
+        assertEquals("foo", one.foo());
+    }
+
+    @Test
+    public void testBoth() throws Exception {
+        One one = new One();
+        assertEquals("toto", one.toto());
+        assertEquals("foo", one.foo());
+    }
 }
